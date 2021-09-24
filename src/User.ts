1089450d1173c9bb25563780146d012ae5124dcc
Jay Hernandez <jeric614@yahoo.com>
2021-09-25 08:32:50 +0900

    exported user

--- src/User.ts
+++ src/User.ts
@@ -1,1 +1,21 @@
-class User {}
+//type definition file f/adapting js library, use files from definitely typed/npm
+import faker from 'faker';
+
+//if export like this then import using { User }
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  //initilization
+  //command click library to get pseudo documentation
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      //faker library returns string so convert with parseFloat
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('hi')
+//start up w/ parcel index.html
+import { User } from './User';
+
+const user = new User();
+
+console.log(user);
