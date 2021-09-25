b306f2d3575e6455d1970699394433abe17832f7
Jay Hernandez <jeric614@yahoo.com>
2021-09-25 11:00:50 +0900

    created custom map to limit functionality of googleClassMap

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
 //start up w/ parcel index.html
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from '../CustomMap';
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
-
-new google.maps.Map(document.querySelector('#map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+new CustomMap('map');
